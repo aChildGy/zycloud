55c4e920b257e27246ebb21110864bfe10f77bb8
shuyou.liu <shuyou.liu@zymobi.com>
2016-10-13 10:03:25 +0800

    erm/inventory.erm

--- user-service/src/main/java/demo/config/AuthorizationServerConfig.java
+++ user-service/src/main/java/demo/config/AuthorizationServerConfig.java
@@ -1,22 +1,97 @@
 package demo.config;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserCache;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
 public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final Logger log = LoggerFactory.getLogger(AuthorizationServerConfig.class);
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    	log.debug("-------------------------------------configureGlobal----------------------------------");
+    	UserCache uc = new UserCache() {
+			private List <UserDetails> users = new ArrayList<UserDetails>();
+			@Override
+			public void removeUserFromCache(String username) {
+				log.debug("-------------------------------------username----------------------------------:"+username);
+				
+			}
+			
+			@Override
+			public void putUserInCache(UserDetails user) {
+				log.debug("-------------------------------------username----------------------------------:"+user.getPassword()
+				+user.getUsername());
+				users.add(user);
+			}
+			
+			@Override
+			public UserDetails getUserFromCache(String username) {
+				UserDetails ud = new UserDetails() {
+					
+					@Override
+					public boolean isEnabled() {
+						return true;
+					}
+					
+					@Override
+					public boolean isCredentialsNonExpired() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					
+					@Override
+					public boolean isAccountNonLocked() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					
+					@Override
+					public boolean isAccountNonExpired() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					
+					@Override
+					public String getUsername() {
+						
+						return "xx";
+					}
+					
+					@Override
+					public String getPassword() {
+						
+						return "xx";
+					}
+					
+					@Override
+					public Collection<? extends GrantedAuthority> getAuthorities() {
+						// TODO Auto-generated method stub
+						return null;
+					}
+				};
+				
+				return ud;
+			}
+		};
+    	
+        auth.jdbcAuthentication().userCache(uc);
     }
 
     @Override
